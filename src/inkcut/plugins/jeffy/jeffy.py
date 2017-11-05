f65a2c34b41ccb48149543907cb21c75dd65ef9a
Jethro <Jethro@LAPTOP-4H48GHPC>
2017-11-05 09:25:54 -0500

    Updated Comments and Removed Legacy Code

--- src/inkcut/plugins/jeffy/jeffy.py
+++ src/inkcut/plugins/jeffy/jeffy.py
@@ -11,112 +11,54 @@ from inkcut.workbench.preferences.plugin import Model
 from contextlib import contextmanager
 
 
-
 class StepperMotor(Model):
     DIR_POS = 1
     DIR_NEG = -1
     
-    delay = Float(0.000001) # time in seconds to hold the output at the set value
-    driver_pins = Tuple()
-    enable_pin = Int()
+    delay = Float(0.000001)                                 # Default Software Square Wave Time Delay
+    driver_pins = Tuple()                                   # StepperMotor Class driver GPIO board pins
+    enable_pin = Int()                                      # StepperMotor Class enable GPIO board pins
     enabled = Bool()
-    steps = Int(0) # Steps relative to starting position
+    steps = Int(0)                                          # Steps relative to starting position
     
-    #_cur_step = Int(0) # Index of step sequence array
-    #step_sequence = List(default=[(1,0,1,0),
-    #                 (0,0,1,0),
-    #                 (0,1,1,0),
-    #                 (0,1,0,0),
-    #                 (0,1,0,1),
-    #                 (0,0,0,1),
-    #                 (1,0,0,1),
-    #                 (1,0,0,0)]) # output values for half stepping
-        
     def __init__(self,*args,**kwargs):
         super(StepperMotor, self).__init__(*args,**kwargs)
         self.init_rpi()
         
     def init_rpi(self):
-        # Init pins
-        pins = list(self.driver_pins) + [self.enable_pin]
-        GPIO.setup(pins, GPIO.OUT, initial=GPIO.LOW)    # set initial value option (1 or 0)
+        pins = list(self.driver_pins) + [self.enable_pin]   # Setup RPi GPIO pins
+        GPIO.setup(pins, GPIO.OUT, initial=GPIO.LOW)        # set initial value option (1 or 0)
         
-    
-    def _observe_enabled(self,change):
-        """ Enable motor for duration of this block """
-        if self.enabled:
+    def _observe_enabled(self,change):                      # Enable motor for duration of this block
+        if self.enabled:                                    
             GPIO.output(self.enable_pin, GPIO.HIGH)
         else:
             GPIO.output(self.enable_pin, GPIO.LOW)
     
     def step(self, steps):
-        ds = 0 if steps<0 else 1#self.DIR_NEG or self.DIR_POS
-        #with self.enabled():
-        #print(self,steps)
+        ds = 0 if steps<0 else 1                            # set ds to 0 or 1 for direction pin output
+        
         for i in xrange(abs(int(steps))):
-                GPIO.output(self.driver_pins, [ds,1])     # set an output port/pin value to 1/GPIO.HIGH/True
-                time.sleep(self.delay)
-                GPIO.output(self.driver_pins, [ds,0])     # set an output port/pin value to 1/GPIO.HIGH/True
-                time.sleep(self.delay)
-
-
-            
-##    def step_legacy(self,steps):
-##        """ Step the motor steps times. Direction depends on sign of steps.
-##        
-##        @param steps: number of steps to take
-##        @yields delay: how long to wait before continuing
-##        """
-##        ds = steps<0 and self.DIR_NEG or self.DIR_POS
-##        
-##        for i in xrange(abs(steps)):
-##            self._cur_step = (self._cur_step + ds) % len(self.step_sequence)
-##            output = self.step_sequence[self._cur_step]
-##            self._set_output(output)
-##            self.steps +=ds # Update total step count
-##            yield self.delay
-##            #if self.delay>0:
-##            #    time.sleep(self.delay) 
-            
-    def _set_output(self,output):
-        """ You should not use this directly! 
-        Sets the output pins to the given value.
-        
-        Example:
-            pins = [1,2,3,4]
-            value = 8 --> 0b1000
-            
-            will set pins to following outputs:
-            pin:    1    2    3    4
-            lvl:    1    0    0    0
-            
-            value = 5 --> 0b0101
-            
-            will set pins to following outputs:
-            pin:    1    2    3    4
-            lvl:    0    1    0    1
-        
-        
-        """
-        GPIO.output(self.driver_pins, output)     # set an output port/pin value to 1/GPIO.HIGH/True
-        
-            
+                GPIO.output(self.driver_pins, [ds,1])       # Set GPIO output (Direction pin to ds, Pulse Pin High) 
+                time.sleep(self.delay)                      # Software Square Wave High Time
+                GPIO.output(self.driver_pins, [ds,0])       # Set GPIO output (Direction pin to ds, Pulse Pin Low) 
+                time.sleep(self.delay)                      # Software Square Wave Low Time
+                  
 class JeffyDevice(Device):
-    bounds = [[0,0],[0,0]]
-    motor = Dict(default={})
-    motor_enable_pins = Tuple(int,default=(3,8))
+    bounds = [[0,0],[0,0]]                                  # End Bounds for Inkcut # TODO: Implement this with actual cutter end bounds        
+    motor = Dict(default={})                                # ??? JAIRUS ???
+    motor_enable_pins = Tuple(int,default=(3,8))            # (X Driver Enable GPIO Pin, Y Driver Enable GPIO Pin)
     motor_driver_pins = Tuple(tuple,default=(
-                        (7, 5),              ## [X Direction Pin, X direction Step Pulse Pin]
-                       (12, 10)))            ## [Y Direction Pin, Y direction Step Pulse Pin]
-    servo_gpio_pins = Tuple(int,default=(19,17))    ## Probably blade up and down
-    boundary_gpio_pins = Tuple(int,default=(        ## Boundary switch input pins
-                          (5,6),
-                          (11,12)))
-    boundary_bounce_timeout = Int(300) # ms         ## Bounce timer for switches
+                        (7, 5),                             # (X Direction Pin, X direction Step Pulse Pin)
+                       (12, 10)))                           # (Y Direction Pin, Y direction Step Pulse Pin)
+    servo_gpio_pins = Tuple(int,default=(19,17))            # TODO: need to connect these pins to cutter blade outputs
+    boundary_gpio_pins = Tuple(int,default=(                # TODO: need to connect these boundary pins to end stop switches
+                          (5,6),                            # (-X switch GPIO boundary Pin, +X boundary switch GPIO pin)  #TODO: connect hardware switches to these pins
+                          (11,12)))                         # (-Y switch GPIO boundary Pin, +Y boundary switch GPIO pin)  #TODO: connect hardware switches to these pins
+    boundary_bounce_timeout = Int(300)                      # Bounce timer for switches [ms] # TODO: integrate timer with end stop switches
     
-    
-    scale = Tuple(default=(36.4,36.4))            # DO NOT LOSE THE MAGIC NUMBER
-    delay = Float(0.0035)
+    scale = Tuple(default=(36.4,36.4))                      # MAGIC VOODOO NUMBER AKA [pixels/motor steps] could also be flipped? not sure  
+    delay = Float(0.0035)                                   # Time Delay for Software Implmented Square Wave #TODO: integrate Real-Time Clock to replace software square wave
 
     _position = List(default=[0,0])
     
@@ -130,8 +72,6 @@ class JeffyDevice(Device):
         self.log.info("Jeffy Device connected boss")
         self.init_rpi()
         self.init_motors()
-        
-
         self.connectionMade()
 
     def disconnect(self):
@@ -147,8 +87,7 @@ class JeffyDevice(Device):
         # Left, Top, Right, Bottom#job.
         #job.media.padding = [x, y, 0, 0]
 
-        #: Updating this does not update the UI
-        self._position = [0,0] # Clear
+        self._position = [0,0] # Clear                    #: Updating this does not update the UI
 
         return job.model
 
@@ -202,7 +141,6 @@ class JeffyDevice(Device):
         #self.log.debug("Move: to ({},{},{}) from ({}) (abs {})".format(dx,dy,z,self.position, absolute))
         dx,dy = int(dx*self.scale[0]), int(dy*self.scale[1])
               
-        
         if absolute:
             #raise NotImplementedError
             dx -= self._position[0]
@@ -213,7 +151,6 @@ class JeffyDevice(Device):
         
         sx = dx>0 and 1 or -1
         sy = dy>0 and 1 or -1
-        #scale = 1021/90.0
         epsilon = 0.000001
         
         fxy = abs(dx)-abs(dy)
@@ -229,50 +166,33 @@ class JeffyDevice(Device):
                     mx,my = sx,0
                     fxy -= abs(dy)
                 
-                # TODO: Interleave steps to make it smoother??
-                # TODO: Set position on each step
                 self.motor[0].step(mx)
-                #for delay in self.motor[0].step(mx):
-                #    if delay:
-                #        time.sleep(delay)
                 self.motor[1].step(my)
-                #for delay in self.motor[1].step(my):
-                #    if delay:
-                #        time.sleep(delay)
-                
+          
                 x += mx
                 y += my
                 
-                # Update position 
                 self._position = [(self._position[0]+mx),(self._position[1]+my),self.position[2]]
                 #self.log.debug("x={} dx={}, y={} dy={}".format(x,dx,y,dy))
-                if x==dx and y==dy: #abs(x-dx) < epsilon and abs(y-dy) < epsilon:
+                if x==dx and y==dy:
                     break
                 
-                #time.sleep(self.delay)
         except KeyboardInterrupt:
              self.disconnect()
              raise
-        #self.position = [dx, dy, z]
-        
-        #return self.position
+        #self.position = [dx, dy, z]        # Udpate for Inkcut Real-Time Update
+        #return self.position               # Update for Inkcut Real-Time Update
     
-    def set_velocity(self, v):
-        # TODO: Map v to delay
+    def set_velocity(self, v):                           # TODO: Map v to delay
         Device.set_velocity(self, v)
     
-    def check_bounds(self):
-        # Move -x until we hit min x bound pin
-        for i in xrange(self.MAX_X):
+    def check_bounds(self):                              # TODO: Develop cutter range check with end stop switches       
+        for i in xrange(self.MAX_X):                     # Move -x until we hit min x bound pin
             self.move(-1,0)
-        
-        # Move -y until we hit min y bound pin
-        for i in xrange(self.MAX_Y):
+        for i in xrange(self.MAX_Y):                     # Move -y until we hit min y bound pin
             self.move(0,-1)
-        
-        # Move +x until we hit max x bound pin
-        
-        # Move +y until we hit max y bound pin
+            # Move +x until we hit max x bound pin
+            # Move +y until we hit max y bound pin
         pass
     
     def on_hit_bound_min_x(self):
@@ -287,8 +207,6 @@ class JeffyDevice(Device):
     def on_hit_bound_max_y(self):
         self.position[1] = self.MAX_Y
         
-
-
 
 # class JeffyProtocol(IDeviceProtocol):
 #     """ Hooks into UI """ 
