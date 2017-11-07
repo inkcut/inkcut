8dce4c15ae11b4e0fe200085c47c3d30b2d8d765
Jethro <JethroJMartin@gmail.com>
2017-11-06 22:55:43 -0500

    Integrated QR Registration into Jeffy.py

--- src/inkcut/plugins/jeffy/jeffy.py
+++ src/inkcut/plugins/jeffy/jeffy.py
@@ -9,7 +9,12 @@ from atom.api import Instance,List,Int,Float,Tuple,Dict, Bool
 from inkcut.workbench.core.device import Device
 from inkcut.workbench.preferences.plugin import Model
 from contextlib import contextmanager
-
+from sys import argv
+from PIL import Image
+from matplotlib import pyplot as plt
+import cv2
+import numpy
+import zbar
 
 class StepperMotor(Model):
     DIR_POS = 1
@@ -80,13 +85,10 @@ class JeffyDevice(Device):
     
     def init(self, job):        
 
-        #: Do QRCode stuff
-        #rotation,x,y = QRcodeWork.register()
-
-        #job.rotation = rotation
+        x,y,rotation = self.QR_Registration()
+        job.rotation = rotation
         # Left, Top, Right, Bottom#job.
-        #job.media.padding = [x, y, 0, 0]
-
+        job.media.padding = [x, y, 0, 0]                  # TODO: Will not work. Translation is not the same as padding
         self._position = [0,0] # Clear                    #: Updating this does not update the UI
 
         return job.model
@@ -207,7 +209,115 @@ class JeffyDevice(Device):
     def on_hit_bound_max_y(self):
         self.position[1] = self.MAX_Y
         
-
+    def QR_Registration(self,SKU = "VF207" ,CameraImage = "QRimageWithGlobals.jpg"):
+        imageRaw = cv2.imread(CameraImage)
+        scanner = zbar.ImageScanner()                                        # create a reader
+        scanner.parse_config('enable')                                       # configure Reader
+        gray = cv2.cvtColor(imageRaw, cv2.COLOR_RGB2GRAY,dstCn=0)            # Obtain image data
+        ret, gray = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)            # Threshold to convert image to binary
+        
+        #gray = cv2.adaptiveThreshold(gray, 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,17,2)          ## May need integrate this later. Using adaptive gaussian method. Useful for images with ranges of shading in image. Good lighting is probably a better implementation
+        
+        pil = Image.fromarray(gray)
+        width, height = pil.size
+        raw = pil.tobytes()
+        image = zbar.Image(width,height,'Y800',raw)                          # wrap image data
+        scanner.scan(image)    
+        
+        
+        for symbol in scanner.results:
+            print(symbol.data)
+            print(symbol.location)
+            A=symbol.location
+            y1a=A[0][1]+0.0                                                  # Corner 0 y point   points to form line a
+            y2a=A[2][1]+0.0                                                  # Corner 2 y point
+            x1a=A[0][0]+0.0                                                  # Corner 0 x point
+            x2a=A[2][0]+0.0                                                  # Corner 2 x point
+        
+            ma = (y2a-y1a)/(x2a-x1a)                                         # Slope of line a
+            ba = y1a - ma*x1a                                                # Intercept of line a
+        
+            y1b=A[1][1]+0.0                                                  # Corner 1 y point   points to form line b
+            y2b=A[3][1]+0.0                                                  # Corner 3 y point
+            x1b=A[1][0]+0.0                                                  # Corner 1 x point
+            x2b=A[3][0]+0.0                                                  # Corner 3 x point    
+        
+            mb = (y2b-y1b)/(x2b-x1b)                                         # Slope of line b
+            bb = y1b - mb*x1b                                                # Intercept of line b
+        
+            xintersect = (bb-ba)/(ma-mb)                                     # x intersection point of lines a and b
+            yintersect = ma*xintersect + ba                                  # y intersection point of lines a and b
+            cv2.circle(imageRaw,(int(xintersect),int(yintersect)),30,(255,0,0),-1)      # draw circles on image for visualization
+        
+            if (symbol.data == 'CutterTopLeft'):                                  
+                CutterTopLeft = (xintersect, yintersect)                     # Save center points of cutter QR codes
+            if (symbol.data == 'CutterTopRight'):
+                CutterTopRight = (xintersect, yintersect)
+            if (symbol.data == 'CutterBottomLeft'):
+                CutterBottomLeft = (xintersect, yintersect)
+            if (symbol.data == 'CutterBottomRight'):
+                CutterBottomRight = (xintersect, yintersect)
+            if (symbol.data == SKU+'topLeft'):
+                SKUtopLeft = (xintersect, yintersect)                        # Save center points of media QR codes
+            if (symbol.data == SKU+'topRight'):
+                SKUtopRight = (xintersect, yintersect)
+            if (symbol.data == SKU+'bottomLeft'):
+                SKUbottomLeft = (xintersect, yintersect)
+            if (symbol.data == SKU+'bottomRight'):
+                SKUbottomRight = (xintersect, yintersect)
+        
+        
+        
+        ##### CUTTER TRANSLATION AND ROTATION CALCULATIONS ########
+        
+        ma = (CutterBottomRight[1] - CutterTopLeft[1] + 0.0)/(CutterBottomRight[0]-CutterTopLeft[0] + 0.0)     # Slope of line a
+        ba = CutterTopLeft[1] - ma*CutterTopLeft[0] + 0.0                                                      # Intercept of line a  
+        
+        mb = (CutterTopRight[1] - CutterBottomLeft[1] + 0.0)/(CutterTopRight[0] - CutterBottomLeft[0] + 0.0)   # Slope of line b
+        bb = CutterBottomLeft[1] - mb*CutterBottomLeft[0] + 0.0                                                # Intercept of line b
+        
+        xintersect = (bb-ba)/(ma-mb)                                                                           # x intersection point of lines a and b
+        yintersect = ma*xintersect + ba                                                                        # y intersection point of lines a and b
+        
+        CutterTranslation = (xintersect , yintersect)                                                          # Cutter Translation , # Let our rotation metric be the sum of slopes of QR code diagonals
+        CutterRotation = (CutterBottomRight[1]-CutterTopLeft[1])/(CutterBottomRight[0]-CutterTopLeft[0]) + (CutterTopRight[1]-CutterBottomLeft[1])/(CutterTopRight[0]-CutterBottomLeft[0]) + 0.0
+        
+        cv2.line(imageRaw,(int(CutterTopLeft[0]),int(CutterTopLeft[1])),(int(CutterBottomRight[0]),int(CutterBottomRight[1])),(255,0,0),5)
+        cv2.line(imageRaw,(int(CutterBottomLeft[0]),int(CutterBottomLeft[1])),(int(CutterTopRight[0]),int(CutterTopRight[1])),(255,0,0),5)
+        
+        cv2.line(imageRaw,(int(SKUtopLeft[0]),int(SKUtopLeft[1])),(int(SKUbottomRight[0]),int(SKUbottomRight[1])),(0,0,255),5)
+        cv2.line(imageRaw,(int(SKUbottomLeft[0]),int(SKUbottomLeft[1])),(int(SKUtopRight[0]),int(SKUtopRight[1])),(0,0,255),5)
+        
+        
+        
+        ######### SKU TRANSLATION AND ROTATION CALCULATIONS #########
+        
+        ma = (SKUbottomRight[1] - SKUtopLeft[1] + 0.0)/(SKUbottomRight[0]-SKUtopLeft[0] + 0.0)                 # Slope of line a
+        ba = SKUtopLeft[1] - ma*SKUtopLeft[0] + 0.0                                                            # Intercept of line a  
+        
+        mb = (SKUtopRight[1] - SKUbottomLeft[1] + 0.0)/(SKUtopRight[0] - SKUbottomLeft[0] + 0.0)               # Slope of line b
+        bb = SKUbottomLeft[1] - mb*SKUbottomLeft[0] + 0.0                                                      # Intercept of line b
+        
+        xintersect = (bb-ba)/(ma-mb)                                                                           # x intersection point of lines a and b
+        yintersect = ma*xintersect + ba                                                                        # y intersection point of lines a and b
+        
+        SKUtranslation = (xintersect , yintersect)                                                             # Cutter Translation , # Let our rotation metric be the sum of slopes of QR code diagonals
+        SKUrotation = (SKUbottomRight[1]-SKUtopLeft[1])/(SKUbottomRight[0]-SKUtopLeft[0]) + (SKUtopRight[1]-SKUbottomLeft[1])/(SKUtopRight[0]-SKUbottomLeft[0]) +0.0
+        
+        
+        
+        #### TODO: need someway of calibrating Cutter to Image ????????? J
+         
+        
+        ## Matplot lib stuff for visualization  # TODO: replace matplotlib with light weight image viewer
+        plt.imshow(imageRaw)
+        plt.show()
+        x=0                                     # TODO: Solve x translation
+        y=0                                     # TODO: Solve y translation
+        rotation=0                              # TODO: Solve rotation
+        return (x, y, rotation)
+    
+    
 # class JeffyProtocol(IDeviceProtocol):
 #     """ Hooks into UI """ 
 #     plotter = Instance(Plotter2D)
