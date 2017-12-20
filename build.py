b5cbf2833c94fb32c53737505f3e6abe52254aab
frmdstryr <frmdstryr@gmail.com>
2017-12-20 13:56:35 -0500

    Add build

---
+++ build.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2017, Jairus Martin.
+
+Distributed under the terms of the GPL v3 License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+Created on Dec 15, 2017
+
+@author: jrm
+"""
+
+
+def main():
+    pass #: TODO
+
+
+if __name__ == '__main__':
+    main()
+
