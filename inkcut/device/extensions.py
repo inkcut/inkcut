e1e344ffcea731835e3e57058e4106fef9d59dd5
frmdstryr <frmdstryr@gmail.com>
2018-04-21 19:11:39 -0400

    Refactor device initialization for #86, fix serial settings not saving

--- inkcut/device/extensions.py
+++ inkcut/device/extensions.py
@@ -19,16 +19,27 @@ DEVICE_PROTOCOL_POINT = 'inkcut.device.protocols'
 DEVICE_TRANSPORT_POINT = 'inkcut.device.transport'
 
 
-def default_device_factory(default_config):
+def default_device_factory(driver, transports, protocols):
     """ Generates a device if none is given by the driver.
+    Parameters
+    ----------
+    driver: DeviceDriver
+        The declaration of the device driver that was selected by the user
+    transports: List[DeviceTransport]
+        A list of DeviceTransport declarations that this device driver supports 
+    protocols: List[DeviceProtocol]
+        A list of DeviceProtocol declarations that this device driver supports 
 
     Returns
     -------
-        result: Device
-            A configured Device that the application can use.
+    result: Device
+        A configured Device that the application can use.
     """
     from .plugin import Device, DeviceConfig
-    return Device(config=DeviceConfig(**default_config))
+    return Device(declaration=driver,
+                  transports=transports,
+                  protocols=protocols,
+                  config=DeviceConfig(**driver.get_device_config()))
 
 
 def default_device_config_view_factory():
@@ -45,23 +56,22 @@ def default_config_view_factory():
 
 class DeviceDriver(Declarative):
     """ Provide meta info about this device """
-    # ID of the device
-    # If none exits one i created from manufacturer.model
+    #: ID of the device. If none exits one is created from manufacturer.model
     id = d_(Unicode())
 
-    # Name of the device (optional)
+    #: Name of the device (optional)
     name = d_(Unicode())
 
-    # Model of the device (optional)
+    #: Model of the device (optional)
     model = d_(Unicode())
 
-    # Manufacturer of the device (optional)
+    #: Manufacturer of the device (optional)
     manufacturer = d_(Unicode())
 
-    # Width of the device (required)
+    #: Width of the device (required)
     width = d_(Unicode())
 
-    # Length of the device, if it uses a roll, leave blank
+    #: Length of the device, if it uses a roll, leave blank
     length = d_(Unicode())
 
     # Factory to construct the inkcut.device.plugin.Device or subclass.
@@ -81,15 +91,41 @@ class DeviceDriver(Declarative):
     #: Default settings to contribute to the config when selected
     default_config = d_(Dict())
 
+    def get_device_config(self):
+        """ Pull the default device config params from the default_config 
+        """
+        cfg = self.default_config.copy()
+        for k in ('connection', 'protocol', 'job'):
+            cfg.pop(k, None)
+        return cfg
+
+    def get_job_config(self):
+        """ Pull the default device config params from the default_config 
+        """
+        return self.default_config.get('job', {}).copy()
+
+    def get_connection_config(self, id):
+        """ Pull the connection config params from the default_config 
+        for the given transport id.
+        """
+        cfg = self.default_config.get('connection', {}).copy()
+        return cfg.get(id, {})
+
+    def get_protocol_config(self, id):
+        """ Pull the protocol config from the default_config """
+        cfg = self.default_config.get('protocol', {}).copy()
+        return cfg.get(id, {})
+
+
 class DeviceProtocol(Declarative):
-    # Id of the protocol
+    #: Id of the protocol
     id = d_(Unicode())
 
-    # Name of the protocol (optional)
+    #: Name of the protocol (optional)
     name = d_(Unicode())
 
-    # Factory to construct the protocol,
-    # takes a single argument for the transport
+    #: Factory to construct the protocol. It receives the DeviceDriver
+    #: as the first argument and the DeviceProtocol declaration as the second
     factory = d_(Callable())
 
     #: Config view for editing the config of this device
@@ -97,14 +133,14 @@ class DeviceProtocol(Declarative):
 
 
 class DeviceTransport(Declarative):
-    # Id of the protocol
+    #: Id of the transport
     id = d_(Unicode())
 
-    # Name of the protocol (optional)
+    #: Name of the transport (optional)
     name = d_(Unicode())
 
-    # Factory to construct the protocol,
-    # takes a single argument for the transport
+    #: Factory to construct the transport. It receives the DeviceDriver
+    #: as the first argument and the DeviceProtocol declaration as the second
     factory = d_(Callable())
 
     #: Config view for editing the config of this device
