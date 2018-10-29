d1f1a87edace5f1f45b0c56fcbcb97a29a409aff
frmdstryr <frmdstryr@protonmail.com>
2018-10-29 09:19:10 -0400

    Update to use enamlx GraphicsView api

--- inkcut/preview/plugin.py
+++ inkcut/preview/plugin.py
@@ -9,58 +9,54 @@ Created on Jul 12, 2015
 
 @author: jrm
 """
-import pyqtgraph as pg
 from atom.api import List, Instance, Enum, Bool, Range
 from enaml.qt import QtCore, QtGui
+from enaml.qt.QtGui import QPainterPath, QTransform
+from enamlx.widgets.api import Pen
+
 from inkcut.core.api import Plugin, Model, unit_conversions, log
-from .plot_view import PainterPathPlotItem
-
-
-QPen = QtGui.QPen
 
 
 class PreviewModel(Model):
     #: List of plot items to display
-    plot = List()
+    items = List(dict)
 
     #: Internal paths for drawing
-    paths = List(QtGui.QPainterPath)
+    paths = List(QPainterPath)
 
     #: Colors
-    pen_media = Instance(QPen)
-    pen_media_padding = Instance(QPen)
-    pen_up = Instance(QPen)
-    pen_offset = Instance(QPen)
-    pen_down = Instance(QPen)
-    pen_device = Instance(QPen)
+    pen_media = Instance(Pen)
+    pen_media_padding = Instance(Pen)
+    pen_up = Instance(Pen)
+    pen_offset = Instance(Pen)
+    pen_down = Instance(Pen)
+    pen_device = Instance(Pen)
 
     def _default_pen_media(self):
-        return pg.mkPen((128, 128, 128))
+        return Pen(color="rgb(128, 128, 128)")
 
     def _default_pen_media_padding(self):
-        return pg.mkPen((128, 128, 128), style=QtCore.Qt.DashLine)
+        return Pen(color="rgb(128, 128, 128)", line_style="dash")
 
     def _default_pen_device(self):
-        return pg.mkPen((235, 194, 194), style=QtCore.Qt.DashLine)
+        return Pen(color="rgb(235, 194, 194)", line_style="dash")
 
     def _default_pen_up(self):
-        return pg.mkPen(hsv=(0.53, 1, 0.5, 0.5))
+        return Pen(color="#00d0ff")
 
     def _default_pen_offset(self):
-        return pg.mkPen(hsv=(0.43, 1, 0.5, 0.5))
+        return Pen(color="#ff00b2")
 
     def _default_pen_down(self):
-        return pg.mkPen((128, 128, 128))
+        return Pen(color="rgb(128, 128, 128)")
 
     def init(self, view_items):
-        default_items = []
-        self.paths = [QtGui.QPainterPath(), QtGui.QPainterPath()]
-
-        default_items.append(PainterPathPlotItem(
-            self.paths[0], pen=self.pen_down))
-        default_items.append(PainterPathPlotItem(
-            self.paths[1], pen=self.pen_up))
-        self.plot = default_items + view_items
+        self.paths = [QPainterPath(), QPainterPath()]
+        default_items = [
+            {'path': self.paths[0], 'pen': self.pen_down},
+            {'path': self.paths[1], 'pen': self.pen_up},
+        ]
+        self.items = default_items+view_items
 
     def update(self, position):
         """ Watch the position of the device as it changes. """
@@ -70,11 +66,11 @@ class PreviewModel(Model):
         if z:
             self.paths[0].lineTo(x, -y)
             self.paths[1].moveTo(x, -y)
-            self.plot[0].updateData(self.paths[0])
+            self.items[0].updateData(self.paths[0])
         else:
             self.paths[0].moveTo(x, -y)
             self.paths[1].lineTo(x, -y)
-            self.plot[1].updateData(self.paths[1])
+            self.items[1].updateData(self.paths[1])
 
 
 class PreviewPlugin(Plugin):
@@ -86,7 +82,7 @@ class PreviewPlugin(Plugin):
     live_preview = Instance(PreviewModel, ())
 
     #: Transform applied to all view items
-    transform = Instance(QtGui.QTransform)
+    transform = Instance(QTransform)
 
     show_grid_x = Bool().tag(config=True)
     show_grid_y = Bool().tag(config=True)
@@ -97,7 +93,7 @@ class PreviewPlugin(Plugin):
         """ Qt displays top to bottom so this can be used to flip it.
 
         """
-        return QtGui.QTransform.fromScale(1, -1)
+        return QTransform.fromScale(1, -1)
 
     def set_preview(self, *items):
         """ Sets the items that will be displayed in the plot
@@ -108,12 +104,7 @@ class PreviewPlugin(Plugin):
             A list of kwargs to to pass to each plot item
 
         """
-        t = self.transform
-        view_items = [
-            PainterPathPlotItem(kwargs.pop('path'), **kwargs)
-            for kwargs in items
-        ]
-        self.preview.plot = view_items
+        self.preview.items = [i for i in items]
 
     def set_live_preview(self, *items):
         """ Set the items that will be displayed in the live plot preview.
@@ -126,9 +117,5 @@ class PreviewPlugin(Plugin):
 
 
         """
-        view_items = [
-            PainterPathPlotItem(kwargs.pop('path'), **kwargs)
-            for kwargs in items
-        ]
-        self.live_preview.init(view_items)
+        self.live_preview.init([i for i in items])
 
