a176cffade804fa14b8055c1312fa687842fb588
frmdstryr <frmdstryr@protonmail.com>
2019-04-26 20:18:25 -0400

    Update tests and make crashes go to file for #156

--- inkcut/core/plugin.py
+++ inkcut/core/plugin.py
@@ -9,10 +9,38 @@ Created on Jul 12, 2015
 
 @author: jrm
 """
+import os
 import enaml
 import traceback
 from inkcut.core.api import Plugin, log
 
+ERROR_TEMPLATE = """
+<html>
+<h3>Sorry, an error prevented Inkcut from starting</h3>
+<p>
+This is either a bug or a required library may be missing on your system.
+As always you can request help on the forums at
+<a href="https://inkcut.org">https://inkcut.org</a>.
+</p>
+
+<p>
+First try to restart the application. If the error persists you can try
+clearing the saved settings in <a href="{config_dir}">{config_dir}</a>
+and restart the application again</a>.
+</p>
+
+<p>
+If that still doesn't work please report a new issue at
+<a href="https://github.com/codelv/inkcut">https://github.com/codelv/inkcut</a>
+with the following error:
+</p>
+
+<pre>{error}</pre>
+
+<p> and include the logs from <a href="{log_dir}">{log_dir}</a>.
+</p>
+</html>
+"""
 
 class CorePlugin(Plugin):
 
@@ -42,12 +70,13 @@ class CorePlugin(Plugin):
             log.error(traceback.format_exc())
             tb = traceback.format_exc().split("\n")
             msg = "\n".join(tb[max(-len(tb), -11):])
-            self.workbench.message_critical("Workspace startup error",
-                "An error prevented startup. This is either a bug or a "
-                "required library is missing on your system. "
-                "\n\n{}\n\n"
-                "Please report this issue or request help at "
-                "https://github.com/codelv/inkcut ".format(msg.strip()))
+            config_dir = os.path.dirname(self._state_file)
+            log_dir = os.path.join(config_dir, 'logs')
+            self.workbench.message_critical(
+                "Workspace startup error",
+                ERROR_TEMPLATE.format(config_dir=config_dir,
+                                      error=msg.strip(),
+                                      log_dir=log_dir))
             raise
 
     def init_logging(self):
@@ -56,4 +85,4 @@ class CorePlugin(Plugin):
         from twisted.python.log import PythonLoggingObserver
         observer = PythonLoggingObserver()
         observer.start()
-        
+
