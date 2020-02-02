63cf9e1e6acb6040c8a72573692386e66640e2c9
Mario Voigt <mariovoigt89@t-online.de>
2020-02-02 02:48:16 +0100

    adding back recent language files for german translation

---
+++ inkcut/res/translations/de_DE.ts
@@ -0,0 +1,638 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>File</name>
+    <message>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+</context>
+<context>
+    <name>control</name>
+    <message>
+        <source>Control</source>
+        <translation>Steuerung</translation>
+    </message>
+    <message>
+        <source>Lift head up</source>
+        <translation>Kopf anheben</translation>
+    </message>
+    <message>
+        <source>Put head down</source>
+        <translation>Kopf absenken</translation>
+    </message>
+    <message>
+        <source>Set the virtual origin</source>
+        <translation>Virtuellen Ursprung setzen</translation>
+    </message>
+    <message>
+        <source>move up</source>
+        <translation>Nach oben bewegen</translation>
+    </message>
+    <message>
+        <source>Return to virtual origin</source>
+        <translation>Zum virtuellen Ursprung zurückkehren</translation>
+    </message>
+    <message>
+        <source>move left</source>
+        <translation>Nach links bewegen</translation>
+    </message>
+    <message>
+        <source>move down</source>
+        <translation>Nach unten bewegen</translation>
+    </message>
+    <message>
+        <source>move right</source>
+        <translation>Nach rechts bewegen</translation>
+    </message>
+    <message>
+        <source>Return to the system origin (0,0)</source>
+        <translation>Zum Systemursprung zurückkehren (0,0)</translation>
+    </message>
+</context>
+<context>
+    <name>core</name>
+    <message>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+</context>
+<context>
+    <name>device</name>
+    <message>
+        <source>Device</source>
+        <translation>Gerät</translation>
+    </message>
+    <message>
+        <source>Setup...</source>
+        <translation>Setup...</translation>
+    </message>
+    <message>
+        <source>Configure device</source>
+        <translation>Gerät konfigurieren</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <source>Driver</source>
+        <translation>Treiber</translation>
+    </message>
+    <message>
+        <source>Manufacturer</source>
+        <translation>Hersteller</translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation>Modell</translation>
+    </message>
+    <message>
+        <source>Width</source>
+        <translation>Breite</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Länge</translation>
+    </message>
+    <message>
+        <source>Customize</source>
+        <translation>Anpassen</translation>
+    </message>
+    <message>
+        <source>Connection</source>
+        <translation>Verbindung</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <source>Protocol</source>
+        <translation>Protokoll</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Send to device</source>
+        <translation>An Gerät senden</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Speed</source>
+        <translation>Geschwindigkeit</translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation>Aktiviert</translation>
+    </message>
+    <message>
+        <source>Force</source>
+        <translation>Kraft</translation>
+    </message>
+    <message>
+        <source>Output</source>
+        <translation>Output</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation type="vanished">Rotation</translation>
+    </message>
+    <message>
+        <source>Scale</source>
+        <translation>Skalieren</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Erweitert</translation>
+    </message>
+    <message>
+        <source>Uses spooling service</source>
+        <translation>Spooling-Dienst verwenden</translation>
+    </message>
+    <message>
+        <source>Interpolate paths</source>
+        <translation>Pfade interpolieren</translation>
+    </message>
+    <message>
+        <source>Step size</source>
+        <translation>Schrittgröße</translation>
+    </message>
+    <message>
+        <source>Step time</source>
+        <translation>Schritt Zeit</translation>
+    </message>
+    <message>
+        <source>Test mode</source>
+        <translation>Testmodus</translation>
+    </message>
+    <message>
+        <source>Job commands</source>
+        <translation>Job Befehle</translation>
+    </message>
+    <message>
+        <source>Before job</source>
+        <translation>Vor Job</translation>
+    </message>
+    <message>
+        <source>Commands here will get sent before starting a job</source>
+        <translation>Befehle hier werden gesendet, bevor ein Job gestartet wird</translation>
+    </message>
+    <message>
+        <source>After job</source>
+        <translation>Nach JobAprès la tâche</translation>
+    </message>
+    <message>
+        <source>Commands here will get sent after finishing a job</source>
+        <translation>Befehle hier werden nach Beendigung eines Jobs gesendet</translation>
+    </message>
+    <message>
+        <source>Connection commands</source>
+        <translation>Verbindungsbefehle</translation>
+    </message>
+    <message>
+        <source>After connect</source>
+        <translation>Nach Verbindung</translation>
+    </message>
+    <message>
+        <source>Commands here will get sent right after a connection is opened</source>
+        <translation>Befehle hier werden direkt nach dem Öffnen einer Verbindung gesendet</translation>
+    </message>
+    <message>
+        <source>Before disconnect</source>
+        <translation>Vor Verbindungstrennung</translation>
+    </message>
+    <message>
+        <source>Commands here will get sent right before a connection is closed</source>
+        <translation>Befehle hier werden direkt gesendet, bevor eine Verbindung geschlossen wird</translation>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>Live</translation>
+    </message>
+    <message>
+        <source>Abort job</source>
+        <translation>Job abbrechen</translation>
+    </message>
+    <message>
+        <source>View all</source>
+        <translation>Einpassen</translation>
+    </message>
+    <message>
+        <source>Clear plot</source>
+        <translation>Plot leeren</translation>
+    </message>
+    <message>
+        <source>Abort the current job</source>
+        <translation>Aktuellen Job abbrechen</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Fortsetzen</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Error starting job</source>
+        <translation>Fehler beim Starten des Jobs</translation>
+    </message>
+    <message>
+        <source>No file is open! Please open a file first.</source>
+        <translation>Es ist keine Datei geöffnet. Bitte erst Datei öffnen.</translation>
+    </message>
+    <message>
+        <source>No device is setup! Please select Device-&gt;Setup from the menu.</source>
+        <translation>Es wurde kein Gerät konfigurieren. Bitte wählen Sie im Menu ein Gerät unter Gerät-&gt;Setup.</translation>
+    </message>
+    <message>
+        <source>No configuration available.</source>
+        <translation>Keine Konfiguration verfügbar.</translation>
+    </message>
+    <message>
+        <source>Orientation</source>
+        <translation>Orientierung</translation>
+    </message>
+    <message>
+        <source>Swap X/Y</source>
+        <translation>X/Y umkehren</translation>
+    </message>
+    <message>
+        <source>Mirror X</source>
+        <translation>X Spiegeln</translation>
+    </message>
+    <message>
+        <source>Mirror Y</source>
+        <translation>Y Spiegeln</translation>
+    </message>
+    <message>
+        <source>Quality factor</source>
+        <translation>Qualitätsfaktor</translation>
+    </message>
+</context>
+<context>
+    <name>filters</name>
+    <message>
+        <source>Blade Offset</source>
+        <translation>Klingenversatz</translation>
+    </message>
+    <message>
+        <source>Set to 0 to disable</source>
+        <translation>0 setzen zum Deaktivieren</translation>
+    </message>
+    <message>
+        <source>Cutoff angle</source>
+        <translation>Schnittwinkel</translation>
+    </message>
+    <message>
+        <source>Angle at minimum angle that must be met to apply compensation</source>
+        <translation>Winkel, der erreicht werden muss, um Kompensation anzuwenden  (im Minimalwinkel)</translation>
+    </message>
+    <message>
+        <source>Overcut</source>
+        <translation>Überschnitt</translation>
+    </message>
+</context>
+<context>
+    <name>job</name>
+    <message>
+        <source>Graphic Rotation</source>
+        <translation>Grafik Rotation</translation>
+    </message>
+    <message>
+        <source>Rotate to save space</source>
+        <translation>Rotieren zum Platzsparen</translation>
+    </message>
+    <message>
+        <source>Graphic Mirroring</source>
+        <translation>Grafik spiegeln</translation>
+    </message>
+    <message>
+        <source>Mirror about x-axis</source>
+        <translation>Um X-Achse spiegeln</translation>
+    </message>
+    <message>
+        <source>Mirror about y-axis</source>
+        <translation>Um Y-Achse spiegeln</translation>
+    </message>
+    <message>
+        <source>Graphic Spacing</source>
+        <translation>Grafik Abstand</translation>
+    </message>
+    <message>
+        <source>Plotting Order</source>
+        <translation>Plott Reihenfolge</translation>
+    </message>
+    <message>
+        <source>Plot Weedlines</source>
+        <translation>Jätelinien</translation>
+    </message>
+    <message>
+        <source>Add weedline to plot</source>
+        <translation>Jätelinien zu Plott  hinzufügen</translation>
+    </message>
+    <message>
+        <source>Add weedline to copies</source>
+        <translation>Jätelinien zu Kopien hinzufügen</translation>
+    </message>
+    <message>
+        <source>Weedline Padding</source>
+        <translation>Jätelinien Abstand</translation>
+    </message>
+    <message>
+        <source>Plot Area</source>
+        <translation>Plott Fläche</translation>
+    </message>
+    <message>
+        <source>Plot Margins</source>
+        <translation>Plott Ränder</translation>
+    </message>
+    <message>
+        <source>Plot Alignment</source>
+        <translation>Plott Anordnung</translation>
+    </message>
+    <message>
+        <source>Plot Feeding</source>
+        <translation>Plott Vorschub</translation>
+    </message>
+    <message>
+        <source>Return to origin</source>
+        <translation>Zum Ursprung zurückkehren</translation>
+    </message>
+    <message>
+        <source>Feed after</source>
+        <translation>Vorschub nach</translation>
+    </message>
+    <message>
+        <source>Jobs</source>
+        <translation>Jobs</translation>
+    </message>
+    <message>
+        <source>Open job</source>
+        <translation>Job öffnen</translation>
+    </message>
+    <message>
+        <source>Send job to device</source>
+        <translation>Job an Maschine senden</translation>
+    </message>
+    <message>
+        <source>Remove from list</source>
+        <translation>Von der Liste entfernen</translation>
+    </message>
+    <message>
+        <source>Graphic Size</source>
+        <translation>Grafik Größe</translation>
+    </message>
+    <message>
+        <source>Lock aspect ratio</source>
+        <translation>Seitenverhältnis sperren</translation>
+    </message>
+    <message>
+        <source>Fill to save space</source>
+        <translation>Ausfüllen zum Platzsparen</translation>
+    </message>
+    <message>
+        <source>Graphic</source>
+        <translation>Grafik</translation>
+    </message>
+    <message>
+        <source>Graphic Copies</source>
+        <translation>Grafik Kopien</translation>
+    </message>
+    <message>
+        <source>Approve Job</source>
+        <translation>Job bestätigen</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to start?</source>
+        <translation>Sind Sie sicher, dass Sie starten wollen?</translation>
+    </message>
+    <message>
+        <source>Job</source>
+        <translation>Job</translation>
+    </message>
+    <message>
+        <source>Name of the document</source>
+        <translation>Dateiname</translation>
+    </message>
+    <message>
+        <source>Duration</source>
+        <translation>Dauer</translation>
+    </message>
+    <message>
+        <source>Estimated job duration based on given speed and the job size.</source>
+        <translation>Geschätzte Job-Dauer, basierend auf gegebener Geschwindigkeit und Job-Größe.</translation>
+    </message>
+    <message>
+        <source>Material</source>
+        <translation>Material</translation>
+    </message>
+    <message>
+        <source>Estimated material usage</source>
+        <translation>Geschätzter Materialbedarf</translation>
+    </message>
+    <message>
+        <source>Don&apos;t ask again</source>
+        <translation>Nicht erneut nachfragen</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbrechnen</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Weedlines</source>
+        <translation>Jätelinien</translation>
+    </message>
+</context>
+<context>
+    <name>pi</name>
+    <message>
+        <source>Enable pin</source>
+        <translation>Activer pin</translation>
+    </message>
+    <message>
+        <source>Direction pin</source>
+        <translation>Pin direction</translation>
+    </message>
+    <message>
+        <source>Step pulse pin</source>
+        <translation>Step pulse pin</translation>
+    </message>
+    <message>
+        <source>Config</source>
+        <translation>Réglages</translation>
+    </message>
+    <message>
+        <source>Step delay</source>
+        <translation>Temps de pause</translation>
+    </message>
+    <message>
+        <source>Pinouts</source>
+        <translation>Pin de sortie</translation>
+    </message>
+    <message>
+        <source>Stepper motor pinout</source>
+        <translation>Stepper motor pinout</translation>
+    </message>
+    <message>
+        <source>Servo pinout</source>
+        <translation>Servo pinout</translation>
+    </message>
+    <message>
+        <source>De-bounce timeout</source>
+        <translation>De-bounce timeout</translation>
+    </message>
+    <message>
+        <source>Scale</source>
+        <translation>Echelle</translation>
+    </message>
+    <message>
+        <source>Registration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Crop mark registration</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>preview</name>
+    <message>
+        <source>Preview</source>
+        <translation>Vorschau</translation>
+    </message>
+    <message>
+        <source>View all</source>
+        <translation>Einpassen</translation>
+    </message>
+</context>
+<context>
+    <name>protocols</name>
+    <message>
+        <source>Mode</source>
+        <translation>Modus</translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Documentation</source>
+        <translation>Dokumentation</translation>
+    </message>
+    <message>
+        <source>Report issue</source>
+        <translation>Fehler berichten</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Spenden</translation>
+    </message>
+    <message>
+        <source>Logs</source>
+        <translation>Logs</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Thema</translation>
+    </message>
+    <message>
+        <source>Job</source>
+        <translation>Job</translation>
+    </message>
+    <message>
+        <source>Default Units</source>
+        <translation>Standardeinheiten</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Vorschau</translation>
+    </message>
+    <message>
+        <source>Grid</source>
+        <translation>Raster</translation>
+    </message>
+    <message>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <source>Alpha</source>
+        <translation>Alpha</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <source>Settings...</source>
+        <translation>Einstellungen...</translation>
+    </message>
+    <message>
+        <source>Forums</source>
+        <translation>Forum</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <source>You must restart for this to apply!</source>
+        <translation>Die Anwendung muss neugestartet werden, damit die Einstellungen wirksam werden!</translation>
+    </message>
+</context>
+</TS>
